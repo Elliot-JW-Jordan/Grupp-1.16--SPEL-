49c5b5586f35a34647a15474bb0a8de58a7b495e
LimeL-bit <emil.flygar@elev.ga.lbs.se>
2024-12-26 19:17:52 +0100

    Made Random Generated dungen
    recomend not to press start !!!
    
    there are some buges.
    - romms cna spawn with bofe doors and walls that are overlaping
    - there is no limit on how meny rooms there are (can crash your unity)

--- Grupp 1.16 Spelprojekt(1)/Assets/Scripts/SpawnPoint.cs
+++ Grupp 1.16 Spelprojekt(1)/Assets/Scripts/SpawnPoint.cs
@@ -4,27 +4,68 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    public int OppeningDirection = 0;
-    // 1 for up
-    // 2 for right 
-    // 3 for down
-    // 4 for left
+    public int OppeningDirection;
+    // 1 for bottom
+    // 2 for top
+    // 3 for left
+    // 4 for right
 
-    private void Update()
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+    private int MaxRooms = 10;
+    private int CurentRooms = 0;
+
+    private void Start()
     {
-        if(OppeningDirection == 1)
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    void Spawn()
+    {
+        if (spawned == false && CurentRooms <= MaxRooms)
         {
-            //spawn a room with at least a door facing down
-        }else if (OppeningDirection == 2)
-        {
-            //spawn a room with at least a door facing left
-        }else if (OppeningDirection == 3)
-        {
-            //spawn a room with at least a door facing up
-        }else if (OppeningDirection == 4)
-        {
-            //spawn a room with at least a door facing right
+            if(OppeningDirection == 1)       
+            {
+                //spawn a room with at least a door facing bottom
+                rand = Random.Range(0, templates.BottomRooms.Length);
+                Instantiate(templates.BottomRooms[rand], transform.position, templates.BottomRooms[rand].transform.rotation);
+                CurentRooms += 1;
+            }
+            else if (OppeningDirection == 2)       
+            {
+                //spawn a room with at least a door facing top
+                rand = Random.Range(0, templates.TopRooms.Length);
+                Instantiate(templates.TopRooms[rand], transform.position, templates.TopRooms[rand].transform.rotation);
+                CurentRooms += 1;
+            }
+            else if (OppeningDirection == 3)       
+            {
+                //spawn a room with at least a door facing left
+                rand = Random.Range(0, templates.LeftRooms.Length);
+                Instantiate(templates.LeftRooms[rand], transform.position, templates.LeftRooms[rand].transform.rotation);
+                CurentRooms += 1;
+            }
+            else if (OppeningDirection == 4)       
+            {
+                //spawn a room with at least a door facing right
+                rand = Random.Range(0, templates.RightRooms.Length);
+                Instantiate(templates.RightRooms[rand], transform.position, templates.RightRooms[rand].transform.rotation);
+                CurentRooms += 1;
+            }
+            spawned = true;
         }
+        
 
     }
+
+    private void OnTriggerEnter2D(Collider2D Other)
+    {
+        if(Other.CompareTag("SpawnPoint") && Other.GetComponent<SpawnPoint>().spawned == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
