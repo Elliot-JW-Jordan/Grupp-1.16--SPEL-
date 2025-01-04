8d2ebde7f32253ca66285091f51a0dd2825132f8
Lime-control <emil.p.flygar@gmail.com>
2025-01-04 23:17:10 +0100

    optemizing

--- Grupp 1.16 Spelprojekt(1)/Assets/Scripts/SpawnPoint.cs
+++ Grupp 1.16 Spelprojekt(1)/Assets/Scripts/SpawnPoint.cs
@@ -14,46 +14,43 @@ public class SpawnPoint : MonoBehaviour
     private RoomTemplates templates;
     private int rand;
     public bool spawned = false;
-    private int MaxRooms = 10;
-    private int CurentRooms = 0;
+
+    public float waitTimeOptemizing = 4f;
 
     private void Start()
     {
+        Destroy(gameObject, waitTimeOptemizing);
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
         Invoke("Spawn", 0.1f);
     }
 
     void Spawn()
     {
-        if (spawned == false && CurentRooms <= MaxRooms)
+        if (spawned == false )
         {
             if(OppeningDirection == 1)       
             {
                 //spawn a room with at least a door facing bottom
                 rand = Random.Range(0, templates.BottomRooms.Length);
                 Instantiate(templates.BottomRooms[rand], transform.position, templates.BottomRooms[rand].transform.rotation);
-                CurentRooms += 1;
             }
             else if (OppeningDirection == 2)       
             {
                 //spawn a room with at least a door facing top
                 rand = Random.Range(0, templates.TopRooms.Length);
                 Instantiate(templates.TopRooms[rand], transform.position, templates.TopRooms[rand].transform.rotation);
-                CurentRooms += 1;
             }
             else if (OppeningDirection == 3)       
             {
                 //spawn a room with at least a door facing left
                 rand = Random.Range(0, templates.LeftRooms.Length);
                 Instantiate(templates.LeftRooms[rand], transform.position, templates.LeftRooms[rand].transform.rotation);
-                CurentRooms += 1;
             }
             else if (OppeningDirection == 4)       
             {
                 //spawn a room with at least a door facing right
                 rand = Random.Range(0, templates.RightRooms.Length);
                 Instantiate(templates.RightRooms[rand], transform.position, templates.RightRooms[rand].transform.rotation);
-                CurentRooms += 1;
             }
             spawned = true;
         }
