74b25cdcf8489a65ed2cda315e5fc688d4d76fc7
LimeL-bit <emil.flygar@elev.ga.lbs.se>
2025-01-10 15:02:25 +0100

    Added and Fixed cammeras

--- Grupp 1.16 Spelprojekt(1)/Assets/Scripts/DoorSpawnScript.cs
+++ Grupp 1.16 Spelprojekt(1)/Assets/Scripts/DoorSpawnScript.cs
@@ -5,10 +5,8 @@ using UnityEngine.UIElements;
 
 public class DoorSpawnScript : MonoBehaviour
 {
+    private int waitTimeOptemizing = 1;
     public int DoorDirection = 0;
-
-    public Vector2 verticalOffset = new Vector2(0f, 6.6f); // Offset for DoorDirection 1 (Up) and 2 (Down)
-    public Vector2 horizontalOffset = new Vector2(10.8f, 0f); // Offset for DoorDirection 3 (Right) and 4 (Left)
 
     private RoomTemplates Doors;
 
@@ -25,23 +23,26 @@ public class DoorSpawnScript : MonoBehaviour
 
         if (DoorDirection == 1)
         {
-            spawnPosition -= verticalOffset;
             Instantiate(Doors.DoorU, spawnPosition, spawnRotation);
+            Destroy(gameObject, waitTimeOptemizing);
         }
+
         if (DoorDirection == 2)
         {
-            spawnPosition += verticalOffset;
             Instantiate(Doors.DoorD, spawnPosition, spawnRotation);
+            Destroy(gameObject, waitTimeOptemizing);
         }
+
         if (DoorDirection == 3)
         {
-            spawnPosition -= horizontalOffset;
             Instantiate(Doors.DoorR, spawnPosition, spawnRotation);
+            Destroy(gameObject, waitTimeOptemizing);
         }
+
         if (DoorDirection == 4)
         {
-            spawnPosition += horizontalOffset;
             Instantiate(Doors.DoorL, spawnPosition, spawnRotation);
+            Destroy(gameObject, waitTimeOptemizing);
         }
 
     }
