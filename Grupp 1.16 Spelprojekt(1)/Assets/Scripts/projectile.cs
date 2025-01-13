416ab340bacd2c96dd5ebceaf18fef7c1070ec43
Benicio8769 <benicio.serrano08@gmail.com>
2025-01-13 22:34:45 +0100

    typ färdig med projectiles, fixar imorgon trust
    Do you like my car?
    My car, my car
    
    Guess you're ready'
    Cause I'm waiting for you
    It's gonna be so exciting!
    Got this feeling
    Really deep in my soul
    Let's get out, I wanna go
    Come along, get it on!
    
    Gonna take my car gonna sit in
    Gonna drive alone till I get you
    'Cause I'm crazy, hot and ready
    But you'll like it! I wanna race for you
    Shall I go now?
    
    Gas gas gas!
    I'm gonna step on the gas
    Tonight I'll fly and be your lover
    Yeah yeah yeah
    I'll be so quick as a flash
    And I'll be your hero
    
    Gas gas gas!
    I'm gonna run as a flash
    Tonight I'll fight to be the winner
    Yeah yeah yeah
    I'm gonna step on the gas
    And you'll see the big show!
    
    Don't be lazy
    'Cause I'm burning for you
    It's like a hot sensation!
    Got this power that is taking me out
    Yes I've got a crush on you
    Ready now ready go!
    
    Gonna take my car
    Do you like my car?
    'Cause I'm crazy, hot and ready
    But you'll like it! I wanna race for you
    Shall I go now?
    
    Gas gas gas!
    I'm gonna step on the gas
    Tonight I'll fly and be your lover
    Yeah yeah yeah
    I'll be so quick as a flash
    And I'll be your hero
    
    Gas gas gas!
    I'm gonna run as a flash
    Tonight I'll fight to be the winner
    Yeah yeah yeah
    I'm gonna step on the gas
    And you'll see the big show!
    
    Gas gas gas!
    Yeah yeah yeah!
    Gas gas gas!
    And you'll see the big show!

--- Grupp 1.16 Spelprojekt(1)/Assets/Scripts/projectile.cs
+++ Grupp 1.16 Spelprojekt(1)/Assets/Scripts/projectile.cs
@@ -1,28 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using CodeMonkey.Utils;
+
 using UnityEngine;
 
 
 
 public class projectile : MonoBehaviour
 {
-    private Transform projectileTransform;
-
-    private void Awake()
-    {
-        projectileTransform = transform.Find("Firepoint");
-    }
+    public GameObject boss1bullet;
+    public float BulletSpeed = 3f;
 
     private void Update()
     {
-        Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
-        Vector3 projectileDirection = mousePosition - (transform.position).normalized;
-        float angle = Mathf.Atan2(projectileDirection.x, projectileDirection.y) * Mathf.Rad2Deg;
+        if (Input.GetKey(KeyCode.E))
+        {
+            shot();
+        }
+    }
+
+    void shot()
+    {
+        GameObject bullet = Instantiate(boss1bullet, transform.position, Quaternion.identity);
+
+        // Get the mouse position in world coordinates
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Correct the z-axis for 2D space
+        mouseWorldPosition.z = 0f;
+
+        // Calculate the direction to the mouse position
+        Vector2 direction = ((Vector2)mouseWorldPosition - (Vector2)bullet.transform.position).normalized;
+
+        // Set the bullet's Rigidbody2D velocity to move toward the mouse position
+        Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+        if (bulletRb != null)
+        {
+            bulletRb.velocity = direction * BulletSpeed;
+        }
     }
 }
 
-    
+
+
+
+
+
+
+
+
+
 
 
 
