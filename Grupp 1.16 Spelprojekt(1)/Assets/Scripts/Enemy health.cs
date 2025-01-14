c8e6e6198b9d7beb6f0ca16eb23b2714ace8e7ab
Elliot-JW-Jordan <elliot.jordan@elev.ga.lbs.se>
2025-01-14 16:34:43 +0100

    minor cahgen

--- Grupp 1.16 Spelprojekt(1)/Assets/Scripts/Enemy health.cs
+++ Grupp 1.16 Spelprojekt(1)/Assets/Scripts/Enemy health.cs
@@ -1,11 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Enemyhealth : MonoBehaviour
 {
     public int health;
     public int maxHealth = 10;
+    public int currencyDrop = 20;
+    public ShopSystem shopSystem1;
+    public UnityEvent<int> OnCurrencyDropped;
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +22,27 @@ public class Enemyhealth : MonoBehaviour
             TakeDamage(5);
         }
     }
-    
+    public void Initialize(ShopSystem shopSystem)
+    {
+        this.shopSystem1 = shopSystem;
+    }
     public void TakeDamage(int amount)
     {
         health -= amount;
         if (health < 0)
         {
+
+         DropCurrency(); //kallar så att spelaren får pengar
             Destroy(gameObject);
+            
         }
 
     }
+
+     void DropCurrency()
+    {
+        OnCurrencyDropped?.Invoke(currencyDrop);
+    }
+
 
 }
