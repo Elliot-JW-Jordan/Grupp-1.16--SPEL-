93608980b2cf508c3ff1cb987a43424325313183
Benicio8769 <benicio.serrano08@gmail.com>
2025-01-14 10:24:11 +0100

    lägg la till health och shoot script i spelaren
    Do you like my car?
    My car, my car
    
    Guess you're ready'
    Cause I'm waiting for you
    It's gonna be so exciting!
    Got this feeling
    Really deep in my soul
    Let's get out, I wanna go
    Come along, get it on!
    
    Gonna take my car gonna sit in
    Gonna drive alone till I get you
    'Cause I'm crazy, hot and ready
    But you'll like it! I wanna race for you
    Shall I go now?
    
    Gas gas gas!
    I'm gonna step on the gas
    Tonight I'll fly and be your lover
    Yeah yeah yeah
    I'll be so quick as a flash
    And I'll be your hero
    
    Gas gas gas!
    I'm gonna run as a flash
    Tonight I'll fight to be the winner
    Yeah yeah yeah
    I'm gonna step on the gas
    And you'll see the big show!
    
    Don't be lazy
    'Cause I'm burning for you
    It's like a hot sensation!
    Got this power that is taking me out
    Yes I've got a crush on you
    Ready now ready go!
    
    Gonna take my car
    Do you like my car?
    'Cause I'm crazy, hot and ready
    But you'll like it! I wanna race for you
    Shall I go now?
    
    Gas gas gas!
    I'm gonna step on the gas
    Tonight I'll fly and be your lover
    Yeah yeah yeah
    I'll be so quick as a flash
    And I'll be your hero
    
    Gas gas gas!
    I'm gonna run as a flash
    Tonight I'll fight to be the winner
    Yeah yeah yeah
    I'm gonna step on the gas
    And you'll see the big show!
    
    Gas gas gas!
    Yeah yeah yeah!
    Gas gas gas!
    And you'll see the big show!

---
+++ Grupp 1.16 Spelprojekt(1)/Assets/Scripts/player Health.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class playerHealth : MonoBehaviour
+{
+    public int health;
+    public int maxHealth = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health < 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
--- Grupp 1.16 Spelprojekt(1)/Assets/Scripts/projectile.cs
+++ Grupp 1.16 Spelprojekt(1)/Assets/Scripts/projectile.cs
@@ -1,37 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
-
-
 
 public class projectile : MonoBehaviour
 {
-    public GameObject boss1bullet;
-    public float BulletSpeed = 3f;
+    public GameObject playerBullet;
+    public float BulletSpeed = 3f; // skottens hastighet
+    public float shotCooldown = 0.5f; // Cooldown i sekunder
+    private float lastShotTime = 0f;
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetMouseButton(0) && Time.time >= lastShotTime + shotCooldown)
         {
             shot();
+            lastShotTime = Time.time; // Uppdatera tiden för senaste skottet
         }
     }
 
     void shot()
     {
-        GameObject bullet = Instantiate(boss1bullet, transform.position, Quaternion.identity);
+        GameObject bullet = Instantiate(playerBullet, transform.position, Quaternion.identity);
 
-        // Get the mouse position in world coordinates
+        // Hämta muspositionen i världskoordinater
         Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        // Correct the z-axis for 2D space
+        // Korrigera z-axeln för 2D-utrymme
         mouseWorldPosition.z = 0f;
 
-        // Calculate the direction to the mouse position
+        // Beräkna riktningen till muspositionen
         Vector2 direction = ((Vector2)mouseWorldPosition - (Vector2)bullet.transform.position).normalized;
 
-        // Set the bullet's Rigidbody2D velocity to move toward the mouse position
+        // Ställ in bullet's Rigidbody2D hastighet för att röra sig mot muspositionen
         Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
         if (bulletRb != null)
         {
@@ -39,16 +39,3 @@ public class projectile : MonoBehaviour
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
