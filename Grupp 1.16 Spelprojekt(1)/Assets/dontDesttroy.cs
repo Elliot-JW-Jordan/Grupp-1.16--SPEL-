61f3d18633620c4d85cb6dd8fa55beecef5783d7
Elliot-JW-Jordan <elliot.jordan@elev.ga.lbs.se>
2025-02-21 14:27:18 +0100

    quickfix

--- Grupp 1.16 Spelprojekt(1)/Assets/dontDesttroy.cs
+++ Grupp 1.16 Spelprojekt(1)/Assets/dontDesttroy.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class dontDesttroy : MonoBehaviour
 {
 
 
-    public static  dontDesttroy instance;
+    public static dontDesttroy instance;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +17,32 @@ public class dontDesttroy : MonoBehaviour
         if (instance == null)
         {
             instance = this;
+            //  den aktiva scenen
+            Scene currentScene = SceneManager.GetActiveScene();
 
             DontDestroyOnLoad(gameObject);
-        } else
-        {
-            Destroy(gameObject); // IFALL EN INSTANS REDAN FINNS, RADERA DENNA;
+
+
+           
         }
-        
-    } 
+        else
+        {
+          Destroy(gameObject); // IFALL EN INSTANS REDAN FINNS, RADERA DENNA;
+        }
+
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Scene currentScene = SceneManager.GetActiveScene(); // den aktive scenen.
+
+        if (currentScene.name == "testStart") // så shopUI inte finns i start
+        {
+            Destroy(gameObject);
+        }
+      
     }
 }
